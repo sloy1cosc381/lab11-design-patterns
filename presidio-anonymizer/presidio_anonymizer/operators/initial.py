397faa3d5c4422ecd16ef30b778823d744eda031
Steven Loy <231585531+sloy1cosc381@users.noreply.github.com>
2025-11-24 04:41:00 +0000

    initial now handles single and mulit words and preserves nonaplhanumeric characters

--- presidio-anonymizer/presidio_anonymizer/operators/initial.py
+++ presidio-anonymizer/presidio_anonymizer/operators/initial.py
@@ -4,12 +4,19 @@ from typing import Dict
 
 from presidio_anonymizer.operators import Operator, OperatorType
 
-class initial(Operator):
+class Initial(Operator):
     """Leave only the initials of the given string"""
 
     def operate(self, text: str = None, params: Dict = None) -> str:
         """:return: the initials of the string."""
-        return (text.search(r'^[^a-zA-Z0-9]*[a-zA-Z0-9]')+". ").upper()
+        def multiword(s):
+            if s==None:
+                return ""
+            else:
+                return s.group()
+        letters=(re.search(r'^[^a-zA-Z0-9]*[a-zA-Z0-9]',text).group()+multiword(re.search(r' [a-zA-Z0-9]',text))).upper()
+        letlist=letters.split()
+        return ". ".join(letlist)+"."
 
     def validate(self, params: Dict = None) -> None:
         """Initial does not require any parameters so no validation is needed."""
